b6c9af0202f58db228623bde95213c50aaa86729
Philippe <pducas1@gmail.com>
2026-03-02 10:42:04 -0500

    fix monsieur repeating

--- script.js
+++ script.js
@@ -25,19 +25,27 @@ function onLoad() {
     console.log("(╯°□°)╯︵ ┻━┻");
 
     monsieursList.forEach(({file, occurrences}, index) => {
-        monsieursList[index].minRange = totalOccurrences;
-        monsieursList[index].maxRange = totalOccurrences + occurrences;
+        monsieursList[index].min = totalOccurrences;
+        monsieursList[index].max = totalOccurrences + occurrences;
         totalOccurrences += occurrences;
     });
 }
+
+let lastRange;
 
 function cliqueMonsieur() {
     monsieursCount += 1;
     document.getElementById('compte-monsieur').innerText = 'Nombre de Monsieurs: ' + monsieursCount;
 
     let magicNumber = Math.random() * totalOccurrences;
+
+    while (lastRange && magicNumber >= lastRange.min && magicNumber < lastRange.max) {
+        magicNumber = Math.random() * totalOccurrences;
+    }
+
     monsieursList.forEach(monsieur => {
-        if (magicNumber >= monsieur.minRange && magicNumber < monsieur.maxRange) {
+        if (magicNumber >= monsieur.min && magicNumber < monsieur.max) {
+            lastRange = { min: monsieur.min, max: monsieur.max };
             document.getElementById('monsieur').src = monsieur.file;
         }
     });
