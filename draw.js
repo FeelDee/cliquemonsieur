a30228fc83c5ce773faeca156cee1ef65b1c1249
Philippe <pducas1@gmail.com>
2026-03-25 17:04:52 -0400

    draw line algorithm

---
+++ draw.js
@@ -0,0 +1,125 @@
+const canvas = document.getElementById('dessine-monsieur');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'white';
+
+const ZOOM_FACTOR = 3;
+
+const pixelsToDraw = [];
+
+function drawPixel(x, y) {
+    // Do not render during mouse events
+    pixelsToDraw.push({ x, y });
+}
+
+function drawLine(x0, y0, x1, y1) {
+
+    if (Math.abs(y1 - y0) < Math.abs(x1 - x0)) {
+        if (x0 > x1) {
+            drawLineLowPitch(x1, y1, x0, y0);
+        }
+        else {
+            drawLineLowPitch(x0, y0, x1, y1);
+        }
+    }
+    else {
+        if (y0 > y1) {
+            drawLineHighPitch(x1, y1, x0, y0);
+        }
+        else {
+            drawLineHighPitch(x0, y0, x1, y1);
+        }
+    }
+}
+
+function drawLineLowPitch(x0, y0, x1, y1) {
+    const dx = x1 - x0;
+    const dy = Math.abs(y1 - y0);
+    const yi = y1 > y0 ? 1 : -1;
+
+    let err = 2 * dy - dx;
+    let y = y0;
+
+    for (let x = x0; x <= x1; x++) {
+        drawPixel(x, y);
+        if (err > 0) {
+            y += yi;
+            err += 2 * (dy - dx);
+        } else {
+            err += 2 * dy;
+        }
+    }
+
+    drawPixel(x1, y1);
+}
+
+function drawLineHighPitch(x0, y0, x1, y1) {
+    const dx = Math.abs(x1 - x0);
+    const dy = y1 - y0;
+    const xi = x1 > x0 ? 1 : -1;
+
+    let err = 2 * dx - dy;
+    let x = x0;
+
+    for (let y = y0; y <= y1; y++) {
+        drawPixel(x, y);
+        if (err > 0) {
+            x += xi;
+            err += 2 * (dx - dy);
+        } else {
+            err += 2 * dx;
+        }
+    }
+
+    drawPixel(x1, y1);
+}
+
+function draw() {
+    while ({ x, y } = pixelsToDraw.shift() ?? false) {
+        ctx.fillRect(x, y, 1, 1);
+    }
+}
+
+let isDrawing = false;
+let drawFunctionIntervalId = -1;
+
+function startDrawing() {
+    isDrawing = true;
+
+    if (drawFunctionIntervalId < 0) {
+        drawFunctionIntervalId = setInterval(draw, 50)
+    }
+}
+
+function stopDrawing() {
+    isDrawing = false;
+    clearInterval(drawFunctionIntervalId);
+    drawFunctionIntervalId = -1;
+
+    // draw remaining pixels
+    draw();
+}
+
+let line = { lastX: 0, lastY: 0 }
+
+canvas.addEventListener('mousedown', (ev) => {
+    startDrawing();
+    const x = Math.floor(ev.offsetX / ZOOM_FACTOR);
+    const y = Math.floor(ev.offsetY / ZOOM_FACTOR);
+    drawPixel(x, y);
+    line.lastX = x;
+    line.lastY = y;
+});
+
+canvas.addEventListener('mousemove', (ev) => {
+    if (!isDrawing) return;
+    const x = Math.floor(ev.offsetX / ZOOM_FACTOR);
+    const y = Math.floor(ev.offsetY / ZOOM_FACTOR);
+    drawLine(line.lastX, line.lastY, x, y)
+    drawPixel(x, y);
+    line.lastX = x;
+    line.lastY = y;
+});
+
+canvas.addEventListener('mouseup', stopDrawing);
+
+canvas.addEventListener('mouseleave', stopDrawing);
